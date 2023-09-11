4a7add06b7d247fc16f6ba35a166ff147b6bf5ab
Hung Do Quoc <hung.do.quoc@student.tuke.sk>
2023-09-11 20:20:31 +0200

    backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   const options = {
     method: 'GET',
-    url: 'https://comment-api-vufj.onrender.com/data',
+    url: 'https://comment-api-backend.onrender.com/data',
     headers: {
       'Content-Type': 'application/json'
     },
