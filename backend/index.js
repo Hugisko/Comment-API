26e814d028f21e946d564af19924f1fe1095e36e
Hung Do Quoc <hung.do.quoc@student.tuke.sk>
2023-09-11 18:24:16 +0200

    first commit

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.listen(port, () => {
+  console.log(`listening at ${port}`);
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await fetch(process.env.FETCH_URL);
+    if (response.ok) {
+      const data = await response.json();
+      res.json({
+        status: "success",
+        body: data,
+      });
+    } else {
+      res.json({
+        status: "failure",
+        body: null,
+        error: "Failed to fetch data from OpenWeatherMap API",
+      });
+    }
+  } catch (error) {
+    res.json({
+      status: 500,
+      body: null,
+      error: error.message,
+    });
+  }
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Loading from './Loading';
+import Comment from "./Comment";
+
+function App() {
+  const [show, setShow] = useState(false);
+  const [comments, setComments] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const options = {
+    method: 'GET',
+    url: 'http://localhost:5000/',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  }
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const resp = await fetch(options.url,options);
+      const data = await resp.json();
+      setComments(data.body);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if(loading){
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  return (
+    <main>
+      <button className="btn" onClick={()=>setShow(!show)}>{show ? "hide" : "show"}</button>
+      <div className="comments__container">
+      {comments.length > 0 && show &&
+        comments.map((comment) => {
+          return <Comment key={comment.id} {...comment} />;
+        })}
+    </div> 
+    </main>
+  );
+}
+
+export default App;
